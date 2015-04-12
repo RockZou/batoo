531fc3d0fce128a093d6f51d398098265de5ff09
Batu <ba921@nyu.edu>
2015-04-12 02:22:16 -0400

    The camera gestures are working. 3 finger to move the camera. Camera boundaries are in order.

--- Assets/_UsedAssets/Scripts/mobileCameraMover.cs
+++ Assets/_UsedAssets/Scripts/mobileCameraMover.cs
@@ -10,76 +10,69 @@ public class mobileCameraMover : MonoBehaviour {
 	private float startZ;
 	private float actualDist;
 	private Vector2 dragStartPos;
+
+	float westBound = 1137.783f;
+	float eastBound = -46.1283f;
+	float northBound = 149.6171f;
+	float southBound = 1391.943f;
+	float inZoomBound= 185.0933f;
+	float outZoomBound = 437.8882f;
+	
+	
+	public float movesensitivityX = 1.0f;
+	public float movesensitivityY = 1.0f;
 	
 	void Start () 
 	{        
-		startZ = projectile.position.z;
 	}
 	
 	void Update () 
 	{
+
+		Touch[] touches = Input.touches;
 		//This section for move the camera position only limited values . 
 		// You can Change the values for your requirements.
 		//Here the camera will move with in your required portion on the screen.
-		
-		if(projectile.position.z >= 100)
-		{            
-			transform.position = transform.position + new Vector3(0.0f,0.0f,-10.0f);
+
+		// This sets the bounderies.
+		if ( projectile.position.x > westBound){
+			projectile.position = new Vector3( westBound, projectile.position.y, projectile.position.z);
 		}
-		else if(projectile.position.z <= -150)
-		{
-			transform.position = transform.position + new Vector3(0.0f,0.0f,10.0f);
+
+		if ( projectile.position.x < eastBound){
+			projectile.position = new Vector3( eastBound, projectile.position.y, projectile.position.z);
 		}
-		
-		if(projectile.position.y >= 140)
-		{
-			transform.position = transform.position + new Vector3(0.0f,-10.0f,0.0f);
+
+		if ( projectile.position.z < northBound){
+			projectile.position = new Vector3( projectile.position.x, projectile.position.y, northBound);
 		}
-		else if(projectile.position.y <= -50)
-		{
-			transform.position = transform.position + new Vector3(0.0f,10.0f,0.0f);
+
+		if ( projectile.position.z > southBound){
+			projectile.position = new Vector3( projectile.position.x, projectile.position.y, southBound);
 		}
-		
-		if(projectile.position.x >= 420)
-		{
-			transform.position = transform.position + new Vector3(-5.0f,0.0f,0.0f);
+
+		if ( projectile.position.y < inZoomBound){
+			projectile.position = new Vector3( projectile.position.x, inZoomBound, projectile.position.z);
 		}
-		else if(projectile.position.x <= 85)
-		{
-			transform.position = transform.position + new Vector3(5.0f,0.0f,0.0f);
+
+		if ( projectile.position.y > outZoomBound){
+			projectile.position = new Vector3( projectile.position.x, outZoomBound, projectile.position.z);
 		}
-		
-		// This Section For to move camera according to your swipe on screen.        
-		if (Input.touchCount == 1 ) 
-		{                
-			Touch touch = Input.GetTouch(0);
-			switch (touch.phase) 
-			{
-			case TouchPhase.Began:
-				dragStartPos = touch.position;
-				moveVec = Vector2.zero;
-				break;
-				
-			case TouchPhase.Moved:
-				Vector3 pos = Camera.main.ScreenToWorldPoint(touch.position);
-				pos.z = startZ;
-				projectile.position = Camera.main.ScreenToWorldPoint(touch.position);
-				//here i gave condition to move camera with in required position 
-				if(projectile.position.z >= -150 && projectile.position.z <= 100)
-				{
-					moveVec = -(touch.position - dragStartPos) * speed;
-				}
-				break;
-				
-			case TouchPhase.Ended:
-				dragStartPos = touch.position;
-				moveVec = Vector2.zero;
-				break;
+
+		// camera movement with touch gestures. 3 fingers
+		if ( Input.touchCount == 3){
+			if (touches[0].phase == TouchPhase.Moved){
+				Vector2 delta = touches[0].deltaPosition;
+
+				float position_x = delta.x * movesensitivityX * Time.deltaTime;
+				float position_y = delta.y * movesensitivityY * Time.deltaTime;
+
+				projectile.position = new Vector3(projectile.position.x + position_x, projectile.position.y , projectile.position.z + position_y);
 			}
-			projectile.Translate(moveVec * Time.deltaTime);
-			Vector3 val = moveVec * Time.deltaTime;
+
 		}
-		
+
+
 		//This section for pinch Zooming on screen.        
 		if (Input.touchCount == 2) 
 		{
